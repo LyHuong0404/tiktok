68b3ea712fe36996454689eb40d99aa5c2ef6721
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-07-02 10:34:00 +0700

    env

--- src/utils/httprequest.js
+++ src/utils/httprequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httprequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 export const get = async (apipath, params = {}) => {
